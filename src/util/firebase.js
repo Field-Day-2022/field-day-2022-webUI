804cf7cec2952ef0a19483162a3a0d5b4cc343ce
jlnorma4 <jlnorma4@asu.edu>
2022-11-15 20:37:25 -0600

    US154-TS155-JNwip, branching Ian to see if I can figure out the test database.

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore, getDocs, collectionGroup } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-<<<<<<< HEAD
-
 const TEST_CONFIG = require("../login.json"); // TODO: revert to deployment database configuration before merge into main
-=======
-const TEST_CONFIG = require('../login.json');
->>>>>>> 18922dec968f07e8fa1f8a62a6c462d68ce34d3c
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
