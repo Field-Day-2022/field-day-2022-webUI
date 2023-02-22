a2044500d6908761877161ecfeaa1d06aaa4f8ae
ilathem <isaiahlathem@gmail.com>
2023-02-21 23:09:25 -0600

    create reusable form builder component

---
+++ src/pages/FormBuilder.jsx
@@ -0,0 +1,10 @@
+import TabBar from "../components/TabBar"
+import PageWrapper from "./PageWrapper"
+
+export const FormBuilder = () => {
+    return (
+        <PageWrapper>
+            <TabBar />
+        </PageWrapper>
+    )
+}
